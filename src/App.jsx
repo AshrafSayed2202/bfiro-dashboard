29dc70e9c745eb76c592b6e83ec7d56c7d21c1bf
Ashraf Sayed <ashraf.neizk@gmail.com>
2026-04-20 13:20:19 +0200

    fix: use dynamic authentication state in ProtectedRoute component

--- src/App.jsx
+++ src/App.jsx
@@ -55,7 +55,7 @@ function App() {
           path="/"
           element={
             //
-            <ProtectedRoute isAuthenticated={true}>
+            <ProtectedRoute isAuthenticated={isAuthenticated}>
               <DashboardLayout />
             </ProtectedRoute>
           }
